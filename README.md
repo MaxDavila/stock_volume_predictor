4dc0ae9ad709c9cc76c1dc6933e9dd9811a811b8
maxdavila <adavila0711@gmail.com>
2013-11-03 15:55:36 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 stock_volume_predictor
 ======================
+This program attempts to predict the change in volume traded for AAPL using Numenta's [online prediction
+framework](https://github.com/numenta/nupic/wiki/Online-Prediction-Framework)
+
+***
+
+### To run 
+
+1. First install [Nupic](https://github.com/numenta/nupic)
+2. Fork the repo into your Vagrant directory
+3. Execute `python stock.py` 
--- stock.py
+++ stock.py
@@ -1,23 +1,4 @@
-# Copyright (C) 2013, Numenta, Inc.  Unless you have purchased from
-# Numenta, Inc. a separate commercial license for this software code, the
-# following terms and conditions apply:
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see http://www.gnu.org/licenses.
-#
-# http://numenta.org/licenses/
-# ----------------------------------------------------------------------
-
-"""A simple client to create a CLA model for hotgym."""
+"""A simple client to predict AAPL's trading volume."""
 
 import csv
 import datetime
@@ -32,7 +13,7 @@ import model_params
 
 _LOGGER = logging.getLogger(__name__)
 
-_DATA_PATH = "table.csv"
+_DATA_PATH = "daily_data.csv"
 
 _METRIC_SPECS = (
     MetricSpec(field='Volume', metric='multiStep',
